604f2dfcee18feb23687bb82724294c8bd4fc686
Aaron Kampmeier <aaron.kampmeier@gmail.com>
2022-10-14 12:19:41 -0700

    Added basic Configuration class code.

---
+++ Configuration.py
@@ -0,0 +1,84 @@
+import numpy as np
+from abc import ABC, abstractmethod
+
+"""
+Use `Configuration` in all code references, but instantiate `StaticObstaclesConfiguration` to begin with.
+"""
+
+
+class Configuration:
+    """
+    Abstract class that represents a game configuration. Implement child classes of this for specific games.
+    Intuition for these methods comes from: https://en.wikipedia.org/wiki/Rapidly-exploring_random_tree
+    """
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_vector(self) -> np.ndarray:
+        """
+        Returns this configuration object encoded as a 1-D array
+        :return:
+        """
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def gen_random_conf() -> "Configuration":
+        """
+        Generates a random _valid_ configuration
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def nearest_vertex(self, vertices: "Configuration") -> "Configuration":
+        """
+        Returns the vertex in the list of vertices that is closest to this one.
+        :param vertices:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def new_conf_from(self, near: "Configuration", delta) -> "Configuration":
+        """
+        Returns a new configuration object that is delta away from "near" conf _towards_ this conf.
+        :param near:
+        :param delta:
+        :return:
+        """
+        pass
+
+
+class StaticObstaclesConfiguration(Configuration):
+    """
+    Configuration for a game where the obstacles are static.
+
+    TODO: Implement this!
+    """
+
+    def __init__(self, agent: (float, float), goal: (float, float)):
+        """
+
+        :param agent:
+        :param goal:
+        """
+        # The state is going to be the (x,y) coord of the agent and the goal
+        self.state = np.array(list(agent) + list(goal))
+        pass
+
+    def get_vector(self) -> np.ndarray:
+        pass
+
+    @staticmethod
+    def gen_random_conf() -> "StaticObstaclesConfiguration":
+        pass
+
+    def nearest_vertex(self, vertices: "StaticObstaclesConfiguration") -> "StaticObstaclesConfiguration":
+        pass
+
+    def new_conf_from(self, near: "StaticObstaclesConfiguration", delta) -> "StaticObstaclesConfiguration":
+        pass
