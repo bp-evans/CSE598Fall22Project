fbe242afc20f7880204795d31cb2961cf9bfd28d
Pavan Kumar Raja <pavanyyf@gmail.com>
2022-10-15 22:14:02 -0700

    Changes for Behavior cloning in RRT

--- BCRRT.py
+++ BCRRT.py
@@ -1,21 +1,56 @@
+import torch
+import DeveloperName
+import numpy as np
+
+from BehaviorCloning import MLP
+from Configuration import DiscreteDirectionAction, StaticObstaclesConfiguration
+
+saved_states = torch.load(DeveloperName.my_name + 'model.pk')
+model = MLP(input_shape=4)
+model.load_state_dict(saved_states['state_dict'])
+model.eval()
+
 
 class BCRRT:
     """
     This is essentially Max's Algorithm 1 from "Efficient Exploration via First-Person Behavior Cloning Assisted Rapidly-Exploring Random Trees"
     """
-    def __init__(self, conf, policy):
+
+    def __init__(self, conf):
         """
         Takes in a BC policy and a start conf
         :param policy:
         """
-        pass
+        self.conf = conf
+        self.step = 5
+        self.G = [conf]
 
-    def run(self, N: int):
+    def run(self, rollout_episodes: int):
         """
         Rolls out the BC policy for N iters starting from conf and builds a tree, then runs RRT on that tree
-        :param N:
+        :param rollout_episodes:
         :return:
         """
-        # rollout policy from conf
-        # run RRT with the seed_tree generated
-        # Return tree/Graph
+        while rollout_episodes:
+            roll_out_depth = 40
+            conf = self.conf
+            while roll_out_depth:
+                x = torch.tensor(conf.as_vector(), dtype=torch.float, requires_grad=False)
+                x = x.view(1, 4)
+                outputs = model(x)
+                _, predicted = torch.max(outputs.data, 1)
+                predicted = int(predicted)
+                predicted += 1  # index start from 0
+                action = DiscreteDirectionAction(predicted)
+                new_conf = StaticObstaclesConfiguration(conf.agent + (20 * action.direction_vector()),
+                                                        conf.goal)
+
+                roll_out_depth -= 1
+                if not conf.is_valid_conf(new_conf):
+                    continue
+                new_conf.parent_vector = (conf, action)
+                self.G.append(new_conf)
+                conf = new_conf
+            rollout_episodes -= 1
+
+        return self.G
--- RunRRTGame.py
+++ RunRRTGame.py
@@ -1,13 +1,28 @@
-from RRTAgent import RRTAgent
+import argparse
+
+
+from BCRRT import BCRRT
+from RRTAgent import RRTAgent, BCRRTAgent
 from ObstacleGame import ObstacleGame
 
-def main():
+
+def main(parsed_args):
     # Run the game with an RRt agent
-    rrt_agent = RRTAgent()
+    if not parsed_args.behavior_cloning:
+        rrt_agent = RRTAgent()
+    else:
+        preset_conf = ObstacleGame.get_start_goal_preset()
+        rrt_agent = BCRRTAgent(BCRRT(preset_conf))
     game = ObstacleGame(rrt_agent)
     game.play(visual=True)
 
 
-if __name__=="__main__":
-    main()
+if __name__ == "__main__":
+    generate_obstacles = False
+    parser = argparse.ArgumentParser(
+        prog='Start RRT',
+        description='Run the game and traverse')
 
+    parser.add_argument('-bc', '--behavior_cloning', action='store_true')
+    args = parser.parse_args()
+    main(args)
