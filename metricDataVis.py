7c552f926877fcbfe4471d12ffb076917111fb8e
Braeden Matthew Woodard <56052736+bmwoodar@users.noreply.github.com>
2022-12-01 04:34:09 -0700

    4-way Data Collection and Testing
    All data files were saved in the Data Collection Folder, CSV files of data collected were uploaded to the drive.

--- metricDataVis.py
+++ metricDataVis.py
@@ -7,36 +7,33 @@ import numpy as np
 from pandas.plotting import parallel_coordinates
 
 # read text file into pandas
-dataRRT = pd.read_csv("RRT_Data.txt", sep=",", header=None, names=["time", "success", "expansions"])
 dataBCRRT = pd.read_csv("BCRRT_Data.txt", sep=",", header=None, names=["time", "success", "expansions"])
-dataIMGBCRRT = pd.read_csv("IMGBCRRT_Data.txt", sep=",", header=None, names=["time", "success", "expansions"])
+dataIMGBCRRT_SA = pd.read_csv("IMGBCRRT_StaticAll.txt", sep=",", header=None, names=["time", "success", "expansions"])
+dataIMGBCRRT_SDO = pd.read_csv("IMGBCRRT_StaticStartGoal_Dynamic_Obs.txt", sep=",", header=None, names=["time", "success", "expansions"])
+dataIMGBCRRT_DA = pd.read_csv("IMGBCRRT_DynamicAll.txt", sep=",", header=None, names=["time", "success", "expansions"])
 
 # Create dataframes
-dfRRT = pd.DataFrame(dataRRT)
 dfBCRRT = pd.DataFrame(dataBCRRT)
-dfIMGBCRRT = pd.DataFrame(dataIMGBCRRT)
+dfIMGBCRRT_SA = pd.DataFrame(dataIMGBCRRT_SA)
+dfIMGBCRRT_SDO = pd.DataFrame(dataIMGBCRRT_SDO)
+dfIMGBCRRT_DA = pd.DataFrame(dataIMGBCRRT_DA)
 ##########################################################################################################
-# Count RRT rows
-rowRRTCount = dfRRT.shape[0]
-#print(rowRRTCount)
-
 # Count BCRRT rows
 rowBCRRTCount = dfBCRRT.shape[0]
+#print(rowRRTCount)
+
+# Count IMGBCRRT_SA rows
+rowIMGBCRRT_SACount = dfIMGBCRRT_SA.shape[0]
 #print(rowBCRRTCount)
 
-# Count BCRRT rows
-rowIMGBCRRTCount = dfIMGBCRRT.shape[0]
+# Count IMGBCRRT_SDO rows
+rowIMGBCRRT_SDOCount = dfIMGBCRRT_SDO.shape[0]
 #print(rowBCRRTCount)
 
-# RRT - Count and filter success data
-successesRRT = dfRRT['success'].value_counts()[True]
-print('Count of True Values in RRT:', successesRRT)
-
-failuresRRT = dfRRT['success'].value_counts()[False]
-print('Count of False Values in RRT:', failuresRRT)
-
-print("The RRT success rate is:", round((successesRRT / rowRRTCount), 2))
-
+# Count IMGBCRRT_DA rows
+rowIMGBCRRT_DACount = dfIMGBCRRT_DA.shape[0]
+#print(rowBCRRTCount)
+##########################################################################
 # BCRRT - Count and filter success data
 successesBCRRT = dfBCRRT['success'].value_counts()[True]
 print('Count of True Values in BCRRT:', successesBCRRT)
@@ -46,50 +43,78 @@ print('Count of False Values in BCRRT:', failuresBCRRT)
 
 print("The BCRRT success rate is:", round((successesBCRRT / rowBCRRTCount), 2))
 
-# IMGBCRRT - Count and filter success data
-successesIMGBCRRT = dfIMGBCRRT['success'].value_counts()[True]
-print('Count of True Values in IMGBCRRT:', successesIMGBCRRT)
+# IMGBCRRT_SA - Count and filter success data
+successesIMGBCRRT_SA = dfIMGBCRRT_SA['success'].value_counts()[True]
+print('Count of True Values in IMGBCRRT_SA:', successesIMGBCRRT_SA)
 
-failuresIMGBCRRT = dfIMGBCRRT['success'].value_counts()[False]
-print('Count of False Values in IMGBCRRT:', failuresIMGBCRRT)
+failuresIMGBCRRT_SA = dfIMGBCRRT_SA['success'].value_counts()[False]
+print('Count of False Values in IMGBCRRT_SA:', failuresIMGBCRRT_SA)
 
-print("The IMGBCRRT success rate is:", round((successesIMGBCRRT / rowIMGBCRRTCount), 2))
+print("The IMGBCRRT_SA success rate is:", round((successesIMGBCRRT_SA / rowIMGBCRRT_SACount), 2))
+
+# IMGBCRRT_SDO - Count and filter success data
+successesIMGBCRRT_SDO = dfIMGBCRRT_SDO['success'].value_counts()[True]
+print('Count of True Values in IMGBCRRT_SDO:', successesIMGBCRRT_SDO)
+
+failuresIMGBCRRT_SDO = dfIMGBCRRT_SDO['success'].value_counts()[False]
+print('Count of False Values in IMGBCRRT_SDO:', failuresIMGBCRRT_SDO)
+
+print("The IMGBCRRT_SDO success rate is:", round((successesIMGBCRRT_SDO / rowIMGBCRRT_SDOCount), 2))
+
+# IMGBCRRT_DA - Count and filter success data
+successesIMGBCRRT_DA = dfIMGBCRRT_DA['success'].value_counts()[True]
+print('Count of True Values in IMGBCRRT_DA:', successesIMGBCRRT_DA)
+
+failuresIMGBCRRT_DA = dfIMGBCRRT_DA['success'].value_counts()[False]
+print('Count of False Values in IMGBCRRT_DA:', failuresIMGBCRRT_DA)
+
+print("The IMGBCRRT_DA  success rate is:", round((successesIMGBCRRT_DA / rowIMGBCRRT_DACount), 2))
 ############################################################################################
-# RRT Time Data
-timeDataRRT = dfRRT['time']
-timeDiffRRT = timeDataRRT.diff()
-print("RRT Time - Mean: "+str(timeDiffRRT.mean())+" Std. Dev: "+str(timeDiffRRT.std()))
-
 # BCRRT Time Data
 timeDataBCRRT = dfBCRRT['time']
 timeDiffBCRRT = timeDataBCRRT.diff()
 print("BCRRT Time - Mean: "+str(timeDiffBCRRT.mean())+" Std. Dev: "+str(timeDiffBCRRT.std()))
 
-# IMGBCRRT Time Data
-timeDataIMGBCRRT = dfIMGBCRRT['time']
-timeDiffIMGBCRRT = timeDataIMGBCRRT.diff()
-print("IMGBCRRT Time - Mean: "+str(timeDiffIMGBCRRT.mean())+" Std. Dev: "+str(timeDiffIMGBCRRT.std()))
+# IMGBCRRT_SA Time Data
+timeDataIMGBCRRT_SA = dfIMGBCRRT_SA['time']
+timeDiffIMGBCRRT_SA = timeDataIMGBCRRT_SA.diff()
+print("IMGBCRRT_SA Time - Mean: "+str(timeDiffIMGBCRRT_SA.mean())+" Std. Dev: "+str(timeDiffIMGBCRRT_SA.std()))
+
+# IMGBCRRT_SDO Time Data
+timeDataIMGBCRRT_SDO = dfIMGBCRRT_SDO['time']
+timeDiffIMGBCRRT_SDO = timeDataIMGBCRRT_SDO.diff()
+print("IMGBCRRT_SDO Time - Mean: "+str(timeDiffIMGBCRRT_SDO.mean())+" Std. Dev: "+str(timeDiffIMGBCRRT_SDO.std()))
+
+# IMGBCRRT_DA Time Data
+timeDataIMGBCRRT_DA = dfIMGBCRRT_DA['time']
+timeDiffIMGBCRRT_DA = timeDataIMGBCRRT_DA.diff()
+print("IMGBCRRT_DA Time - Mean: "+str(timeDiffIMGBCRRT_DA.mean())+" Std. Dev: "+str(timeDiffIMGBCRRT_DA.std()))
 
 ################################################################################
 # Create Box Plot of Time Data
-sns.boxplot(data = [timeDiffRRT,timeDiffBCRRT,timeDiffIMGBCRRT]).set(xlabel="Algorithm", ylabel="Time to Terminal")
-plt.title('RRT vs BCRRT vs IMGBCRRT: Time to Goal')
+sns.boxplot(data = [timeDiffBCRRT,timeDiffIMGBCRRT_SA,timeDiffIMGBCRRT_SDO,timeDiffIMGBCRRT_DA]).set(xlabel="Algorithm", ylabel="Time to Terminal")
+plt.title('BCRRT vs IMGBCRRT_SA vs IMGBCRRT_SDO vs IMGBCRRT_DA: Time to Goal')
 plt.show()
 
 ####################################################################################
-# RRT Expansion Data
-expansionDataRRT = dfRRT['expansions']
-print("RRT Expansion - Mean: "+str(expansionDataRRT.mean())+" Std. Dev: "+str(expansionDataRRT.std()))
-
 # BCRRT Expansion Data
 expansionDataBCRRT = dfBCRRT['expansions']
 print("BCRRT Expansion - Mean: "+str(expansionDataBCRRT.mean())+" Std. Dev: "+str(expansionDataBCRRT.std()))
 
-# IMGBCRRT Expansion Data
-expansionDataIMGBCRRT = dfIMGBCRRT['expansions']
-print("IMGBCRRT Expansion - Mean: "+str(expansionDataIMGBCRRT.mean())+" Std. Dev: "+str(expansionDataIMGBCRRT.std()))
+# IMGBCRRT_SA Expansion Data
+expansionDataIMGBCRRT_SA = dfIMGBCRRT_SA['expansions']
+print("IMGBCRRT_SA Expansion - Mean: "+str(expansionDataIMGBCRRT_SA.mean())+" Std. Dev: "+str(expansionDataIMGBCRRT_SA.std()))
+
+# IMGBCRRT_SDO Expansion Data
+expansionDataIMGBCRRT_SDO = dfIMGBCRRT_SDO['expansions']
+print("IMGBCRRT_SDO Expansion - Mean: "+str(expansionDataIMGBCRRT_SDO.mean())+" Std. Dev: "+str(expansionDataIMGBCRRT_SDO.std()))
+
+# IMGBCRRT_DA Expansion Data
+expansionDataIMGBCRRT_DA = dfIMGBCRRT_DA['expansions']
+print("IMGBCRRT_DA Expansion - Mean: "+str(expansionDataIMGBCRRT_DA.mean())+" Std. Dev: "+str(expansionDataIMGBCRRT_DA.std()))
+
 #############################################################
 # Create Box Plot of Expansion Data
-sns.boxplot(data = [expansionDataRRT,expansionDataBCRRT,expansionDataIMGBCRRT]).set(xlabel="Algorithm", ylabel="Expansions")
-plt.title('RRT vs BCRRT vs IMGBCRRT: Expansions')
+sns.boxplot(data = [expansionDataBCRRT,expansionDataIMGBCRRT_SA,expansionDataIMGBCRRT_SDO,expansionDataIMGBCRRT_DA]).set(xlabel="Algorithm", ylabel="Expansions")
+plt.title('BCRRT vs IMGBCRRT_SA vs IMGBCRRT_SDO vs IMGBCRRT_DA: Expansions')
 plt.show()
