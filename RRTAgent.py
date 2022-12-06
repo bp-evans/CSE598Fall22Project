48a14cb606cc8d3ee348b0e3a4853b445ac734e1
Braeden Matthew Woodard <56052736+bmwoodar@users.noreply.github.com>
2022-12-05 20:19:00 -0700

    Completed Results Section Finishing Inputting Images
    Still in progress in results section

--- RRTAgent.py
+++ RRTAgent.py
@@ -42,7 +42,7 @@ class Observer(RRTObserver):
     def rrt_terminated(self, found_terminal: bool):
         end = time.time()
         timeSec = end - start
-        file1 = open("IMGBCRRT_StaticStartGoal_Dynamic_Obs.txt", "a")
+        file1 = open("IMGBCRRT_StaticAll.txt", "a")
         file1.write(str(timeSec) + "," + str(found_terminal) + "," + str(self.i) + "\n")
         file1.close()
         print(
